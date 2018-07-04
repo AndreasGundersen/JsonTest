3a9f74414fc202dd96c278eabc7eda06edeaf835
Andreas Gundersen <andreas.gundersen@adm.ku.dk>
2018-07-04 15:13:02 +0200

    Adding files

---
+++ StreamReaderTest/Abilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace StreamReaderTest
+{
+    public class Abilities
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+
+        public Abilities()
+        {
+        }
+    }
+}
---
+++ StreamReaderTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace StreamReaderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            using (StreamReader reader = new StreamReader("Abilities.json")) 
+            {
+                line = reader.ReadToEnd();
+            }
+
+            string json = line;
+
+            List<Abilities> abilities = JsonConvert.DeserializeObject<List<Abilities>>(json);
+
+            foreach (var ability in abilities)
+            {
+                Console.WriteLine(ability.Name);
+                Console.WriteLine(ability.Description);
+                Console.WriteLine(ability.ImageUrl);
+            }
+
+
+
+
+
+        }
+    }
+}
